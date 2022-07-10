6deec568fdd94851b67b4ca8f282ac6c5abbcb45
chan._.yy <34885297+Chaaany@users.noreply.github.com>
2022-07-10 17:30:00 +0900

    #feature : add model to admin
    add Doctor, Hospital, TreatmentDepartment, NonpaidCareItem, TreatmmentTime, Profile model to admin

--- doctor/admin.py
+++ doctor/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Doctor, Hospital, NonpaidCareItem, Profile, TreatmentDepartment, TreatmentTime
+
 
 # Register your models here.
+admin.site.register(Doctor)
+admin.site.register(Hospital)
+admin.site.register(TreatmentDepartment)
+admin.site.register(NonpaidCareItem)
+admin.site.register(TreatmentTime)
+admin.site.register(Profile)
